2a768d488bff1f7e7c1aa6c3c2aadd5da794941d
gom3a <mohamed.mido85@azhar.edu.eg>
2021-01-01 17:03:25 +0200

    email testing via mailtrap

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-
+use App\Events\UserCreated;
+use App\Listeners\NotifyUserCreated;
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -17,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        UserCreated::class => [
+            NotifyUserCreated::class,
         ],
     ];
 
