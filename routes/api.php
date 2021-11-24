78f629523500d1fa7125e1ed8d92a392dadeb627
gom3a <mohamed.mido85@azhar.edu.eg>
2021-11-25 00:55:01 +0200

    committing

--- routes/api.php
+++ routes/api.php
@@ -96,4 +96,4 @@ Route::post("publish-name/{identity_id}", "ManagementController@editNameForProdu
 Route::post("preview", "ManagementController@previewProduct");
 Route::get("search", "ProductController@filterProductSearchPage");
 Route::get("products", "ProductController@getAllProducts");
-Route::get('update-option/{product_id}', "ProductController@UpdateProductOptions");
+Route::post('update-option/{product_id}', "ProductController@UpdateProductOptions");
