547d5743eaf951528acc3f5d1a98705e30edcf9e
gom3a <mohamed.mido85@azhar.edu.eg>
2021-12-07 14:41:03 +0200

    committing

--- app/Models/ProductIdentity.php
+++ app/Models/ProductIdentity.php
@@ -25,7 +25,8 @@ class ProductIdentity extends Model
         'product_id', 'name', 'kind', 'city', 'style', 'category', 'material', 'places_tags', 'country', 'shape', 'base', 'seats', 'is_outdoor', 'is_for_kids', 'type',
         'product_file_kind', 'preview_cover', 'preview_price'
     ];
-    public $appends = ['product', 'materials', 'types', 'seatss', 'styles', 'shapess'];
+    // public $appends = ['product', 'materials', 'types', 'seatss', 'styles', 'shapess'];
+    public $appends = ['product'];
     public function product()
     {
         return $this->belongsTo('App\Models\Product');
@@ -38,48 +39,48 @@ class ProductIdentity extends Model
         return $product;
     }
 
-    public function getMaterialsAttribute()
-    {
-        $collection = collect($this->material);
-        $selected_materials = $collection->map(function ($item) {
-            return  ['label' => $item, 'value' => $item];
-        });
-        return $selected_materials->all();
-    }
+    // public function getMaterialsAttribute()
+    // {
+    //     $collection = collect($this->material);
+    //     $selected_materials = $collection->map(function ($item) {
+    //         return  ['label' => $item, 'value' => $item];
+    //     });
+    //     return $selected_materials->all();
+    // }
 
-    public function getTypesAttribute()
-    {
-        $collection = collect($this->type);
-        $selected_types = $collection->map(function ($item) {
-            return  ['label' => $item, 'value' => $item];
-        });
-        return $selected_types->all();
-    }
+    // public function getTypesAttribute()
+    // {
+    //     $collection = collect($this->type);
+    //     $selected_types = $collection->map(function ($item) {
+    //         return  ['label' => $item, 'value' => $item];
+    //     });
+    //     return $selected_types->all();
+    // }
 
-    public function getStylesAttribute()
-    {
-        $collection = collect($this->style);
-        $selected = $collection->map(function ($item) {
-            return  ['label' => $item, 'value' => $item];
-        });
-        return $selected->all();
-    }
+    // public function getStylesAttribute()
+    // {
+    //     $collection = collect($this->style);
+    //     $selected = $collection->map(function ($item) {
+    //         return  ['label' => $item, 'value' => $item];
+    //     });
+    //     return $selected->all();
+    // }
 
-    public function getSeatssAttribute()
-    {
-        $collection = collect($this->seats);
-        $selected = $collection->map(function ($item) {
-            return  ['label' => $item, 'value' => $item];
-        });
-        return $selected->all();
-    }
+    // public function getSeatssAttribute()
+    // {
+    //     $collection = collect($this->seats);
+    //     $selected = $collection->map(function ($item) {
+    //         return  ['label' => $item, 'value' => $item];
+    //     });
+    //     return $selected->all();
+    // }
 
-    public function getShapessAttribute()
-    {
-        $collection = collect($this->shape);
-        $selected = $collection->map(function ($item) {
-            return  ['label' => $item, 'value' => $item];
-        });
-        return $selected->all();
-    }
+    // public function getShapessAttribute()
+    // {
+    //     $collection = collect($this->shape);
+    //     $selected = $collection->map(function ($item) {
+    //         return  ['label' => $item, 'value' => $item];
+    //     });
+    //     return $selected->all();
+    // }
 }
