242e46a5923ebad06cc5b982c22c2c2d6a2cf816
gom3a <mohamed.mido85@azhar.edu.eg>
2022-01-13 09:48:49 +0200

    committing

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,7 +29,7 @@ class Product extends Model
         'updated_at'
     ];
 
-    public $appends = ['identity', 'options', 'description', 'files', 'gallery', 'store'];
+    public $appends = ['identity', 'options', 'description', 'files', 'gallery', 'stores'];
 
     public function images()
     {
@@ -97,7 +97,7 @@ class Product extends Model
         return $this->gallery()->get();
     }
 
-    public function getStoreAttribute()
+    public function getStoresAttribute()
     {
         $store =  DB::table('stores')->where('id', $this->store_id)->first();
         return  $store;
