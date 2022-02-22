0bcc00df599572fa2ae079be1a4bd70bbe1980cd
gom3a <mohamed.mido85@azhar.edu.eg>
2022-02-22 11:18:30 +0200

    committing

--- app/Models/ProductIdentity.php
+++ app/Models/ProductIdentity.php
@@ -19,7 +19,7 @@ class ProductIdentity extends Model
         'base' => 'array',
         'seats' => 'array',
         'type' => 'array',
-        'lighing_types' => 'array',
+        'lighting_types' => 'array',
         'bulbTypes' => 'array',
         'applied_on' => 'array',
         'installations' => 'array',
@@ -29,7 +29,7 @@ class ProductIdentity extends Model
 
     protected $fillable = [
         'product_id', 'name', 'kind', 'city', 'style', 'category', 'material', 'places_tags', 'country', 'shape', 'base', 'seats', 'is_outdoor', 'is_for_kids', 'type',
-        'product_file_kind', 'preview_cover', 'preview_price', 'lighing_types', 'applied_on',  'bulbTypes', 'installations', 'colorTempratures'
+        'product_file_kind', 'preview_cover', 'preview_price', 'lighting_types', 'applied_on',  'bulbTypes', 'installations', 'colorTempratures'
     ];
     public $appends = [
         'product',
