4fa4ac06cf0732a9d08b91d93588cd579b173729
gom3a <mohamed.mido85@azhar.edu.eg>
2022-03-12 21:48:05 +0200

    committing

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -11,7 +11,7 @@ class Folder extends Model
 
     protected $fillable = ['user_id', 'name'];
 
-    protected $appends = ['saved', 'pics'];
+    protected $appends = ['saved'];
 
     public function products()
     {
