85a9380e3dc27e26e8c6276ebf77e064340fb3af
gom3a <mohamed.mido85@azhar.edu.eg>
2022-03-13 00:00:26 +0200

    commiting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -176,12 +176,12 @@ class UserController extends Controller
     {
         $collections = Folder::all()->where('user_id', $user_uid);
         $followrs = Follower::all()->where('follower_id', $user_uid)->first();
-        $stores = $followrs->stores;
+        // $stores = $followrs->stores;
 
         return response()->json([
             'status' => true,
             'collections' =>  $collections,
-            'stores' => $stores
+            'stores' => $followrs
         ], 200);
     }
 }
