a633ff5bb563427e7cb5868bade361d749e0d910
gom3a <mohamed.mido85@azhar.edu.eg>
2022-03-12 21:44:58 +0200

    committing

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -23,25 +23,25 @@ class Folder extends Model
         return false;
     }
 
-    public function getPicsAttributes()
-    {
-        $pics = [];
-        $ids = [];
-        $names = [];
-        $prices = [];
-        $prs = $this->products()->latest()->take(3)->get();
-        foreach ($prs as $pr) {
-            array_push($pics, $pr->identity[0]->preview_cover);
-            array_push($names, $pr->identity[0]->name);
-            array_push($prices, $pr->identity[0]->preview_price);
-            array_push($ids, $pr->identity[0]->product_id);
-        }
-        return [
-            'ids' => $ids,
-            'pics' => $pics,
-            'prices' => $prices,
-            'names' => $names,
-            // 'store' => Store::find($this->store_id)->name
-        ];
-    }
+    // public function getPicsAttributes()
+    // {
+    //     $pics = [];
+    //     $ids = [];
+    //     $names = [];
+    //     $prices = [];
+    //     $prs = $this->products()->latest()->take(3)->get();
+    //     foreach ($prs as $pr) {
+    //         array_push($pics, $pr->identity[0]->preview_cover);
+    //         array_push($names, $pr->identity[0]->name);
+    //         array_push($prices, $pr->identity[0]->preview_price);
+    //         array_push($ids, $pr->identity[0]->product_id);
+    //     }
+    //     return [
+    //         'ids' => $ids,
+    //         'pics' => $pics,
+    //         'prices' => $prices,
+    //         'names' => $names,
+    //         // 'store' => Store::find($this->store_id)->name
+    //     ];
+    // }
 }
