409f44bcd85c92d177f654e68c3f240ddc0f2b1f
gom3a <mohamed.mido85@azhar.edu.eg>
2022-03-13 02:46:05 +0200

    committing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -181,7 +181,7 @@ class UserController extends Controller
         return response()->json([
             'status' => true,
             'collections' =>  $collections,
-            'follower' => $follower->stores->get('name')
+            'follower' => $follower->stores()->get('name')
         ], 200);
     }
 }
